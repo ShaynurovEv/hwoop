5d1513b7e4c7a07479dba9912ad826e31401829b
Zhenya <shaynurov75@gmail.com>
2023-06-30 18:30:30 +0900

    change work

--- src/main/java/ru/netology/homework/Radio.java
+++ src/main/java/ru/netology/homework/Radio.java
@@ -3,9 +3,18 @@ package ru.netology.homework;
 public class Radio {
     private int currentStation;
     private int currentVolume;
+    private int maxStation;
+
+    public Radio() {
+        this.maxStation = 9;
+    }
+
+    public Radio(int stationsCount) {
+        this.maxStation = stationsCount - 1;
+    }
 
     public void next() {
-        if (currentStation !=9) {
+        if (currentStation != maxStation) {
             currentStation++;
         } else {
             currentStation = 0;
@@ -13,10 +22,10 @@ public class Radio {
     }
 
     public void prev() {
-        if (currentStation !=0) {
+        if (currentStation != 0) {
             currentStation--;
         } else {
-            currentStation = 9;
+            currentStation = maxStation;
         }
     }
 
@@ -45,7 +54,7 @@ public class Radio {
         if (currentStation < 0) {
             return;
         }
-        if (currentStation > 9) {
+        if (currentStation > maxStation) {
             return;
         }
         this.currentStation = currentStation;
